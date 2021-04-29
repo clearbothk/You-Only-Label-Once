352e08d65bcbedfa32124f617f75294b99d2cc73
azwinlam <azwin.lam@gmail.com>
2021-04-29 11:02:50 +0800

    cleaned up folder. no images

--- Root/1-Convert_images.py
+++ Root/1-Convert_images.py
@@ -1,15 +1,16 @@
 from tqdm import tqdm
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 
 print("----------")
 print("Converting all images to JPG format for processing")
 print("----------")
 
-raw_data = './raw_data/'
-for pic in tqdm(os.listdir(raw_data)):
+raw_images = './raw_images/'
+for pic in tqdm(os.listdir(raw_images)): 
     if ('.txt' not in pic):
-        name = pic.split('.')        
-        im = Image.open(raw_data + pic)
+        name = pic.split('.')
+        im = Image.open(raw_images + pic)
         im = im.convert('RGB')
+        im = ImageOps.exif_transpose(im)
         im.save('./unlabeled_images/' + name[0] + '.jpg')
---
+++ Root/4-Crop_by_bounding_box.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[344]:
+
+
+import glob
+from tqdm import tqdm
+import shutil
+from PIL import Image
+
+
+# In[345]:
+
+
+txt_dir = glob.glob("./just_bottles/*.txt")
+class_target = ["4"]
+
+
+# In[348]:
+
+
+txt_with_class_target = []
+
+for txt in tqdm(txt_dir):
+    with open(txt, "r") as f:
+        text = f.readlines()
+    for i in range(len(text)):
+        if text[i].split()[0] in class_target:
+            txt_with_class_target.append(txt)
+            break
+
+
+# In[349]:
+
+
+list_of_images = []
+for i in range(len(txt_with_class_target)):
+    test_image = txt_with_class_target[i].split("\\")[1][:-4]
+    list_of_images.append(test_image)
+
+
+# In[350]:
+
+
+list_of_images[:3]
+
+
+# In[356]:
+
+
+#read_txt
+for filename in list_of_images:
+    #Grab Bounding Boxes in Each Txt file
+    bounding_boxes = []
+    with open(f"./just_bottles/{filename}.txt") as f:
+        text = [line.strip().split() for line in f.readlines()]
+        for line in text:
+            if line[0] == "4":
+                convert_to_float = [float(i) for i in line]
+                bounding_boxes.append(convert_to_float)
+#     with Image.open(f"./Alex_data/images/{i}.jpg") as im:
+#         dw,dh = im.size
+#         image = im
+    img = Image.open(f"./just_bottles/{filename}.jpg")
+    for i in bounding_boxes:
+        _, x, y, w, h = i
+
+        left = int((x - w / 2) * dw)
+        right = int((x + w / 2) * dw)
+        top = int((y - h / 2) * dh)
+        bottom = int((y + h / 2) * dh)
+        im_cropped = img.crop((left, top, right, bottom))
+        im_cropped.save(f"./test_delete/{filename}.jpg")
+
+
+# In[ ]:
+
+
+
+
