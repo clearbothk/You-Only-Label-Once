e5dfcd9bd5b2ce93da8f99bf0dea23a356b2f3e8
ahhhlexli <alexli0605@gmail.com>
2021-05-06 11:42:29 +0800

    .

--- Prediction_Pipeline/yolo_check.py
+++ Prediction_Pipeline/yolo_check.py
@@ -1,8 +1,13 @@
 import os
 from subprocess import Popen, PIPE, STDOUT
 
-def clone_yolo():
-    if 'yolov5' not in os.listdir():
+def clone_yolo(YOLO):
+
+    if YOLO[-6:] == 'yolov5':
+        print('YOLOv5 folder found!\n')
+    #if 'yolov5' not in os.listdir(YOLO):
+    else:
+        os.chdir(YOLO)
         print('YOLOv5 not found. Installing...\n')
         os.system("git clone https://github.com/ultralytics/yolov5")
         # url = "https://github.com/ultralytics/yolov5"  # Target clone repo address
@@ -13,5 +18,7 @@ def clone_yolo():
         # for line in proc.stdout:
         #     if line:
         #         print(line.strip())  # Now you get all terminal clone output text
-    else:
-        print('YOLOv5 folder found!\n')
+    # else:
+    #     print('YOLOv5 folder found!\n')
+        YOLO = os.getcwd() + '/yolov5'
+    return YOLO
